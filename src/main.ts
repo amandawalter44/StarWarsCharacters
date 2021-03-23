1e287639ddcb63b60bfb796a46d9d016a54c2755
Amanda Walter <amandawalter44@gmail.com>
2021-03-23 17:06:22 -0400

    Update lib in tsConfig and remove unneeded code

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { CharacterListModule } from './app/character-list/character-list.module';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(CharacterListModule)
   .catch(err => console.error(err));
